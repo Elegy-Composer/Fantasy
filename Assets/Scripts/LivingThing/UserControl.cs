e5b3be855074161e569ecac391671baf77843250
secminhr <secminhrian@gmail.com>
2020-08-15 19:13:49 +0800

    Split user control to standalone script

---
+++ Assets/Scripts/LivingThing/UserControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LivingThing
+{
+    public class UserControl : MonoBehaviour
+    {
+
+        public Rigidbody2D body;
+        public float speed;
+
+        private Vector2 moveVector;
+
+        void Update()
+        {
+            float x = Input.GetAxis("Horizontal");
+            float y = Input.GetAxis("Vertical");
+            moveVector = new Vector2(x, y);
+        }
+
+        void FixedUpdate()
+        {
+            body.MovePosition(body.position + moveVector * speed * Time.fixedDeltaTime);
+        }
+    }
+}
